81c8a3975c4aa9fbdee2ee2cc3400d25ad02af5b
GOKU-21 <gokulcp2004@gmail.com>
2026-08-27 03:19:39 +0530

    GROOVE 1

---
+++ groovy1.js
@@ -0,0 +1,5 @@
+$: note("<[36 48]*4 [34 46]*4 [41 53]*4 [39 51]*4>").gain(rand.range(2, .5)).pianoroll().release(.05) //.sound("sawtooth")
+$: s("bd:2*4,  [rd]")
+$: s("sd!16").gain(.10)
+
+//incoplete
