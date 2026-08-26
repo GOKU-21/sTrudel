b093f6fad7a899ca79acd5d4d93f6254898d1d45
GOKU-21 <gokulcp2004@gmail.com>
2026-08-27 03:18:58 +0530

    do work

--- openingofsmtg.js
+++ openingofsmtg.js
@@ -20,3 +20,5 @@ $: note("48 67 63 [62, 58], [36, 43]") /// start without sawtooth
 
 // note("<[36 48]*4 [34 46]*4 [41 53]*4 [39 51]*4>")
 // .sound("gm_acoustic_bass piano")    /// refer - understand 
+
+//INCOMPLETELY DONE
